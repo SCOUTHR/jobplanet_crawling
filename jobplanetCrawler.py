949a00c7d74aae32410278b6262d452e2a1663f3
OneUne <charming9871@gmail.com>
2021-06-03 16:47:20 +0900

    페이지 로딩 대기 Implicit(무조건) --> Explicit(조건) 으로 변경

--- jobplanetCrawler.py
+++ jobplanetCrawler.py
@@ -1,6 +1,9 @@
 import csv
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def crawlApage():
     driver.implicitly_wait(1)
@@ -23,31 +26,29 @@ def crawlApage():
             info[4] = elems[idx+1]
     # print(info)
     w.writerow(info)
-    # print('끝')
+
 
 driver = webdriver.Chrome(executable_path = r"C:/Users/SCOUT/Downloads/chromedriver_win32/chromedriver.exe")
 
 URL = 'https://www.jobplanet.co.kr/job/search?q=2021+%EC%9A%94%EA%B8%B0%EC%9A%94+%EC%8B%A0%EC%9E%85+%EA%B0%9C%EB%B0%9C%EC%9E%90+%EC%B1%84%EC%9A%A9+%28Rookie+Hero+4%EA%B8%B0%29&posting_ids%5B%5D=1194719&_rs_con=job&_rs_act=popularity_job_postings&_rs_element=job_home_index'
-
+filename = 'test'
 collist = ["회사명","담당자","연락처","이메일","회사위치"]
-
 size = 100 # 열 개 단위로 설정
 
-with open('test.csv','w',-1,newline='',encoding='utf-8-sig') as f:
+
+with open(filename+'.csv','w',-1,newline='',encoding='utf-8-sig') as f:
     w = csv.writer(f)
     w.writerow(collist)
     driver.get(URL)
-    # driver.implicitly_wait(10)
 
     for p in range((size//10)+1):
         for c in range(1,10):
             crawlApage()
-            nextCompany = driver.find_element_by_css_selector('#job_search_app > div > div.job_search_content > div.job_search_list > div.list > ul > li:nth-child(%d)' %(c+1))
+            nextCompany = WebDriverWait(driver, 100).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#job_search_app > div > div.job_search_content > div.job_search_list > div.list > ul > li:nth-child(%d)" %(c+1))))
             nextCompany.click()
         if p in (0,1,2):
             nextPage = driver.find_element_by_css_selector('#job_search_app > div > div.job_search_content > div.job_search_list > div.jply_pagination_ty1 > button:nth-child(%d)' %(p+2))
         else :
             nextPage = driver.find_element_by_css_selector('#job_search_app > div > div.job_search_content > div.job_search_list > div.jply_pagination_ty1 > button:nth-child(6)')
         nextPage.click()
-        #nextPage.send_keys(Keys.ENTER)
-        driver.implicitly_wait(10)
+        # nextPage.send_keys(Keys.ENTER)
